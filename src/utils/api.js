6754de9514706cef87385266f5ce45bcf77b541e
vagrant <vagrant@homestead>
2019-03-12 12:15:05 +0800

    authRequest and logout

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,17 +11,17 @@ const request = async (options, showLoading = true) => {
       url: options
     }
   }
-    // 显示记载中
+  // 显示记载中
   if (showLoading) {
-    wepy.showLoading({ title: '记载中' })
+    wepy.showLoading({ title: '加载中' })
   }
-    // 拼接请求地址
+  // 拼接请求地址
   options.url = host + '/' + options.url
-    // 调用小程序的 request 方法
+  // 调用小程序的 request 方法
   let response = await wepy.request(options)
 
   if (showLoading) {
-        // 隐藏加载中
+    // 隐藏加载中
     wepy.hideLoading()
   }
 
@@ -35,21 +35,21 @@ const request = async (options, showLoading = true) => {
 }
 
 // 登录
-const login = async(params = {}) => {
-    // 获取code
+const login = async (params = {}) => {
+  // 获取code
   let loginData = await wepy.login()
   console.log(loginData)
 
   params.code = loginData.code
 
-    // 接口请求 weapp/authorizations
+  // 接口请求 weapp/authorizations
   let authResponse = await request({
     url: 'weapp/authorizations',
     data: params,
     method: 'POST'
   })
 
-    // 登录成功，记录 token 信息
+  // 登录成功，记录 token 信息
   if (authResponse.statusCode === 201) {
     wepy.setStorageSync('access_token', authResponse.data.access_token)
     wepy.setStorageSync('access_token_expired_at', new Date().getTime() + authResponse.data.expires_in * 1000)
@@ -57,7 +57,88 @@ const login = async(params = {}) => {
   return authResponse
 }
 
-export default{
+// 刷新token
+const refreshToken = async (accessToken) => {
+  // 请求刷新接口
+  let refreshResponse = await wepy.request({
+    url: host + '/' + 'authorizations/current',
+    method: 'PUT',
+    header: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+
+  // 刷新成功
+  if (refreshResponse.statusCode === 200) {
+    // 存储新的token
+    wepy.setStorageSync('access_token', refreshResponse.data.access_token)
+    wepy.setStorageSync('access_token_expired_at', new Date().getTime() + refreshResponse.data.expires_in * 1000)
+  }
+  return refreshResponse
+}
+
+// 获取 Token
+const getToken = async (options) => {
+  // 从缓存重取出 Token
+  let accessToken = wepy.getStorageSync('access_token')
+  let expiredAt = wepy.getStorageSync('access_token_expired_at')
+
+  // 如果 token过期了，则调用刷新方法
+  if (accessToken && new Date().getTime() > expiredAt) {
+    let refreshResponse = await refreshToken(accessToken)
+    // 刷新成功
+    if (refreshResponse.statusCode === 200) {
+      accessToken = refreshResponse.data.access_token
+    } else {
+      let authResponse = await login()
+      if (authResponse.statusCode === 201) {
+        accessToken = authResponse.data.access_token
+      }
+    }
+  }
+  return accessToken
+}
+
+// 带身份认证的请求
+const authRequest = async (options, showLoading = true) => {
+  if (typeof options === 'string') {
+    options = {
+      url: options
+    }
+  }
+  // 获取Token
+  let accessToken = await getToken()
+
+  // 将 Token 设置在header中
+  let header = options.header || {}
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+
+  return request(options, showLoading)
+}
+
+// 退出登录
+const logout = async (params = {}) => {
+  let accessToken = wepy.getStorageSync('access_token')
+  // 调用删除Token接口
+  let logoutResponse = await wepy.request({
+    url: host + '/' + 'authorizations/current',
+    method: 'DELETE',
+    header: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+  // 调用成功
+  if (logoutResponse.statusCode === 204) {
+    wepy.clearStorage()
+  }
+  return logoutResponse
+}
+
+export default {
   request,
-  login
+  login,
+  logout,
+  authRequest,
+  refreshToken
 }
